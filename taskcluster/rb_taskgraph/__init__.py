85da5af33e5979cc9db1c899506c37b32fa15de5
Tom Prince <mozilla@hocat.ca>
2019-09-24 10:31:25 -0600

    decision-paramters example

--- taskcluster/rb_taskgraph/__init__.py
+++ taskcluster/rb_taskgraph/__init__.py
@@ -5,6 +5,9 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
 from importlib import import_module
+from taskgraph.parameters import extend_parameters_schema
+
+from .gradle import get_geckoview_version
 
 
 def register(graph_config):
@@ -13,8 +16,19 @@ def register(graph_config):
     the process.
     """
     _import_modules(["job", "worker_types", "routes", "target"])
+    extend_parameters_schema(
+        {
+            Required("geckoview-version"): text_type,
+            Required("gecko-revision"): text_type,
+        }
+    )
 
 
 def _import_modules(modules):
     for module in modules:
         import_module(".{}".format(module), package=__name__)
+
+
+def get_decision_parameters(graph_config, parameters):
+    parameters["geckoview-version"] = get_geckoview_version()
+    parameters["gecko-revision"] = "b74e5737da64a7af28ab4f81f996950917aa71c5"
